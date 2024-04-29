4adc523ed0d8c340064bc3084041a627e9c7e97e
Varun Kadkade <varunkadkade2442@gmail.com>
2024-04-29 12:35:31 +0530

    ssl cert config

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import uvicorn
 from app import app
 
 if __name__ == "__main__":
-    uvicorn.run(app, port=8000, ssl_keyfile="./private.key", ssl_certfile="./certificate.crt")
+    uvicorn.run(app, port=8000, ssl_keyfile="./private.key", ssl_certfile="./certificate.crt", ssl_ca_certs="./ca_bundle.crt")
